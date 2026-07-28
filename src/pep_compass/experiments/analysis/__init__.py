bb9116addba1a19432255b88daf66ce77895b1ff
Max <maxstrozyk@gmail.com>
2026-07-29 01:14:49 +0200

    Cleaning - removed not finished module

--- src/pep_compass/experiments/analysis/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Lazy loading and analysis of optimization experiment outputs."""
-
-from pep_compass.experiments.analysis.catalog import LocalityExperiment
-from pep_compass.experiments.analysis.selection import ExperimentSelection
-
-__all__ = ["ExperimentSelection", "LocalityExperiment"]
